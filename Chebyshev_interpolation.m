6840005a8488b7680ae7461ada58c268f0f88283
LyudmilYovkov <89297807+LyudmilYovkov@users.noreply.github.com>
2023-01-02 20:16:06 +0200

    Update Chebyshev_interpolation.m

--- Chebyshev_interpolation.m
+++ Chebyshev_interpolation.m
@@ -1,37 +1,35 @@
-%===================================
-% USING THE ZEROF OF THE CHEBISHOV'S
-% POLYNOMIALS FOR INTERPOLATING DATA;
-% IN THIS AND ONLY IN THIS CASE THE 
-% ERROR OF THE APPROXIMATION IS 
-% MINIMAL
-%===================================
-clear;  clc;
-%===================================
-% DATA
-%===================================
+%==========================================%
+% We use the zeros of the Chebyshev        %
+% polynomials to interpolate experimental  %
+% data with minimal possible error.        %    
+%==========================================%
+clear; clc;
+%===================================%
+% Data.                             % 
+%===================================%
 a = 0; b = 1;
-n = input('Enter n = '); % n=12
+n = input('Enter n = ');
 i = 0 : n;
-xc = (a+b)/2 + (b-a)/2 * ...
-    cos((2*i+1)*pi/(2*n+2));
+xc = (a + b) / 2 + (b - a) / 2 * ...
+    cos( (2 * i + 1) * pi / (2 * n + 2) );
 yc = sqrt(xc);
-%===================================
-% CALLING POLYFIT()
-%===================================
+%=====================================%
+% Using polifit() built-in function.  %
+%=====================================%
 p = polyfit(xc,yc,n);
-%===================================
-% FINER GRID
-%===================================
+%===================================%
+% Creating finer spatial grid.      %
+%===================================%
 xx = a : 0.001 : b;
 yy = polyval(p,xx);
 ff = sqrt(xx);
-%===================================
-% PLOT
-%===================================
+%============%
+% Plot.      %
+%============%
 figure(1)
-%%%
+% % %
 subplot(1,2,1)
-%%%
+% % %
 plot(xx,yy,'b','LineWidth',3)
 hold on
 grid on
@@ -44,9 +42,9 @@ legend('\bf{Polynomial p(x)}', ...
        '\bf{Exact f(x)}', ...
        '\bf{Data}')
 title('\bf{Chebyshev interpolation}')
-%%%
+% % %
 subplot(1,2,2)
-%%%
+% % %
 plot(xx,abs(yy-ff),'r:','LineWidth',3)
 hold on
 grid on
@@ -55,11 +53,11 @@ xlabel('x')
 ylabel('y')
 legend('\bf{Error = |f(x)-p(x)|}')
 title('\bf{Error at Chebyshev interpolation}')
-%===================================
-% ERROR
-%===================================
-abs_err = abs(yy-ff);
-rel_err = max(abs_err)./(max(ff)).*100;
+%=======================%
+% Approximation error.  %
+%=======================%
+abs_err = abs(yy - ff);
+rel_err = max(abs_err) ./ (max(ff)) .* 100;
 display('====================================')
 display('Maximal absolute error: ')
 display(max(abs_err))
