15709495bceab5349fb99209e49aead58a762361
LyudmilYovkov <89297807+LyudmilYovkov@users.noreply.github.com>
2023-01-02 20:50:51 +0200

    Update IBVP_1.m

--- IBVP_1.m
+++ IBVP_1.m
@@ -1,89 +1,116 @@
-tic
-%===================================
-% BVP for ODE of 2nd order
-% u'' - u = x
-% u(0) = 0
-% u'(1) = (e^2-e+1) / (e^2);
-% u_{exact} = e^(-x) * (-1 + e^(2*x) - x*e^(x));
-%===================================
-clear all
-clc
-%===================================
-% Grid
-%===================================
-a = 0; b = 1; h = 0.01;
-x = a : h : b;
+%============================================%
+% BVP for ODE of 2nd order                   %
+% u'' - u = x, 0 < x < 1                     %
+% u(0) = 0                                   %
+% u'(1) = (e^2-e+1) / (e^2)                  %
+%============================================%
+% Exact solution for comparison:             %
+% u(x) = e^(-x) * (-1 + e^(2*x) - x*e^(x)).  %
+%============================================%
+
+clear;
+clc;
+
+tic;
+%=================%
+% Spatial grid    %
+%=================%
+a = 0; b = 1;
+x = linspace(a,b,200);
+h = x(2) - x(1);
 N = length(x);
+
+%=================================%
+% Tabulating the exact solution.  %
+%=================================%
+ur = ...
+    exp(-x) .* (-1 + exp(2*x) - x.*exp(x));
+
+%========================%
+% Approximate solution.  %
+%========================%
 y = zeros(1,N);
-%===================================
-% Exact solution
-%===================================
-ur = exp(-x) .* (-1 + exp(2*x) - x.*exp(x));
+
 %===================================
 % Initial condition
 %===================================
-y(1) = 0;
-%===================================
-% adiag
-%===================================
+%y(1) = 0;
+
+%================================%
+% Matrix of the discrete linear  %
+% system of equations.           %
+%================================%
+system_matrix = zeros(N,N);
+
+%=================================%
+% Diagonals of the sparse system  %
+% matrix.                         % 
+%=================================%
+% Lower diagonal adiag.           %
+%=================================%
 adiag = zeros(1,N);
 adiag(1) = 0;
-adiag(end) = -1/h;
 for ii = 2 : N-1
     adiag(ii) = 1;
 end
-%===================================
-% bdiag
-%===================================
+adiag(end) = -1/h;
+
+%=================================%
+% Main diagonal bdiag.            %
+%=================================%
 bdiag = zeros(1,N);
 bdiag(1) = 1;
-bdiag(end) = 1/h + h/2;
 for ii = 2 : N-1
     bdiag(ii) = -(h^2+2);
 end
-%===================================
-% cdiag
-%===================================
+bdiag(end) = 1/h + h/2;
+
+%=================================%
+% Upper diagonal cdiag.           % 
+%=================================%
 cdiag = zeros(1,N);
 cdiag(1) = 0;
-cdiag(end) = 0;
 for ii = 2 : N-1
     cdiag(ii) = 1;
 end
-%===================================
-% ddiag
-%===================================
+cdiag(end) = 0;
+
+%==================================%
+% Right-hand side of the system.   %
+%==================================%
 alpha = (exp(1)^2 - exp(1) + 1) / (exp(1));
-ddiag = zeros(N,1);
-ddiag(1) = 0;
-ddiag(end) = alpha - h/2 * x(end);
+right_side = zeros(N,1);
+right_side(1) = 0;
 for ii = 2 : N-1
-    ddiag(ii) = h^2 * x(ii);
+    right_side(ii) = h^2 * x(ii);
 end
-%===================================
-% Matrix A
-%===================================
-A = zeros(N,N);
+right_side(end) = alpha - h/2 * x(end);
+
+%====================================%
+% Defining the matrix of the system. %
+%====================================%
 for i0 = 1 : N
     for j0 = 1 : N
-        if(i0==j0+1)
-            A(i0,j0) = adiag(i0);
+        if(i0 == j0+1)
+            system_matrix(i0,j0) = adiag(i0);
         end
-        if(j0==i0+1)
-            A(i0,j0) = cdiag(i0);
+        if(j0 == i0+1)
+            system_matrix(i0,j0) = cdiag(i0);
         end
-        if(i0==j0)
-            A(i0,j0) = bdiag(i0);
+        if(i0 == j0)
+            system_matrix(i0,j0) = bdiag(i0);
         end
     end
 end
-%===================================
-% Progonka method
-%===================================
-y = Progon(A,ddiag)';
-%===================================
-% Plot
-%===================================
+
+%===================%
+% Progonka method.  %
+%===================%
+y = Progon(system_matrix, right_side)';
+
+%==========%
+% Plot.    %
+%==========%
 figure(1)
 plot(x,y,'b','LineWidth',3)
 hold on
@@ -91,12 +118,16 @@ grid on
 plot(x,ur,'g--','LineWidth',3)
 xlabel('\bf{x}')
 ylabel('\bf{Solution}')
-legend('\bf{Approximated y}', ...
-    '\bf{Exact u}')
-%===================================
-% Error
-%===================================
-err = abs(y-ur);
-display(['Maximal error: ',num2str(max(err))])
+legend('\bf{Approximated solution}', ...
+       '\bf{Exact solution}')
+title(['\bf{Boundary value problem for ODE }', ...
+       '\bf{of second order with a Neumann }', ...
+       '\bf{boundary condition}'])
+       
+%===================%
+% Absolute error.   %
+%===================%
+err = abs(y - ur);
+display(['Maximal error: ', num2str(max(err))])
 time = toc;
-display(['Elapsed time: ',num2str(time)])
+display(['Elapsed time: ', num2str(time)])
